cfbd2829b2e5290718717c94ff87c798f5750da6
Andrew172 <61161730+Andrew172@users.noreply.github.com>
2020-11-02 16:54:30 +0400

    window

--- Chainword/Chainword/CreateCross.Designer.cs
+++ Chainword/Chainword/CreateCross.Designer.cs
@@ -240,7 +240,7 @@
             this.MinimizeBox = false;
             this.Name = "CreateCross";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn";
+            this.Text = "Создание кроссворда";
             this.Load += new System.EventHandler(this.CreateCross_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
